30a73cd448ba1985cad0e30b68972aa15572424a
Tam Le <tritam.le54@gmail.com>
2025-02-15 23:31:42 +0700

    feat: add config for mason, copilot and vue

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.lang.vue" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     -- import/override with your plugins
     { import = "plugins" },
   },
