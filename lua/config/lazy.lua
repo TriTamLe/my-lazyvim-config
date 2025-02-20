99b1f8a13c8e64b18f53200af98070e78dbd3869
TamLe54 <tam.le@techsoft.asia>
2025-02-20 16:52:43 +0700

    remove telescope

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -42,7 +42,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.util.dot" },
-    { import = "lazyvim.plugins.extras.editor.telescope" },
     -- import/override with your plugins
     { import = "plugins" },
   },
